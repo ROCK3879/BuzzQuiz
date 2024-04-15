b8e0693231b35e89ecc92ee91dc59f586c6d66b3
Prela 9 <amir.prelic@gmail.com>
2024-04-15 14:26:20 +0000

    Adding java script repeating functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,16 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -27,7 +31,7 @@ function showQuestion(question) {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
-        if (answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
@@ -36,22 +40,131 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex +1) { 
+      nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass (element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
     {
-        question: 'How many wings does a bee have?', 
+        question: 'What xxxxxxxxxxxxxxx?', 
         answers: [
-            { text: '4', correct: true },
-            { text: '6', correct: false}
+            { text: 'xxxxx', correct: false},
+            { text: 'xxxxx', correct: false},
+            { text: 'xxxx', correct: false},
+            { text: 'xxxx', correct: true}
         ]
-    }
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'xx', correct: true},
+            { text: 'xx', correct: false},
+            { text: 'xx', correct: false},
+            { text: 'xx', correct: false}
+        ]
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'xxx', correct: false},
+            { text: 'xxx', correct: true},
+            { text: 'xxx', correct: false},
+            { text: 'xxx', correct: false}
+        ]
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'xxcc', correct: false},
+            { text: 'xxcc', correct: false},
+            { text: 'xxcc', correct: true},
+            { text: 'xxcc', correct: false}
+        ]
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'ccvv', correct: false},
+            { text: 'bbvv', correct: true},
+            { text: 'ggvv', correct: false},
+            { text: 'ffdd', correct: false}
+        ]
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'ssss', correct: false},
+            { text: 'dddd', correct: false},
+            { text: 'ffff', correct: false},
+            { text: 'eeee', correct: true}
+        ]
+    },
+    {
+        question: 'What xxxxxxxxxxxxxxx?', 
+        answers: [
+            { text: 'wwss', correct: false},
+            { text: 'gdfr', correct: false},
+            { text: 'pdfpr', correct: true},
+            { text: 'pdse', correct: false}
+        ]
+    },
+    {
+        question: 'What kind of insects are bees?', 
+        answers: [
+            { text: 'Wingless insects', correct: false},
+            { text: 'Winged insects', correct: true },
+            { text: 'Eusocial insects', correct: false},
+            { text: 'Pets', correct: false}
+        ]
+    },
+    {
+        question: 'How many legs does a bee have?', 
+        answers: [
+            { text: 'four', correct: false},
+            { text: 'eight', correct: false },
+            { text: 'six', correct: true},
+            { text: 'two', correct: false}
+        ]
+    },
+    {
+        question: 'How long is the life cycle of a bee?', 
+        answers: [
+            { text: '2-4 years', correct: false },
+            { text: '18-30 days', correct: true },
+            { text: '60-90 days', correct: false },
+            { text: '24 hours', correct: false}
+        ]
+    },
 ]
